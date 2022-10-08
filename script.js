0be330ff1b87d8608507dc43e6b3f52576877295
Diego Lopez <diego.lopez.19@hotmail.es>
2022-10-08 12:59:13 -0600

    implement the hight score and values to display in the screen

--- script.js
+++ script.js
@@ -10,18 +10,29 @@
 // document.querySelecto(".guess").value = 23;
 // console.log(document.querySelector(".guess").value);
 
+const number = Math.trunc(Math.random()* 20) +1;
 
-
+let score = 20;
+document.querySelector(".number") = SecretNumber;
+// Event handler function
 document.querySelector(".check").addEventListener("click", function() {
     const guess = Number( document.querySelector(".guess").value);
     console.log(typeof guess)
 
     if ( !guess ){
         document.querySelector(".message").textContent = " No Number"
-    } else {
-        
+    } else if (guess === SecretNumber) {
+        document.querySelector(".message").textContent = " Correct Number"
+    } else if (guess >  SecretNumber) {
+        document.querySelector(".message").textContent = "😵‍💫 Too high"
+        score --;
+        document.querySelector(".score").textContent = score;
+    } else if ( guess < SecretNumber){
+        document.querySelector(".message").textContent = " image.png Too Low"
+        score --;
+        document.querySelector(".score").textContent = score;
     }
 });
 
 
-// Event handler function
+

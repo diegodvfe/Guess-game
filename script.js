617c813fde5b4a1800c9377a4427c83d04c403c5
Diego Lopez <diego.lopez.19@hotmail.es>
2022-10-08 12:19:15 -0600

    new project

---
+++ script.js
@@ -0,0 +1,18 @@
+'use strict';
+
+// console.log(document.querySelector(".message").textContent);
+
+// document.querySelector(".message").textContent = "🍧 Correct number";
+
+// document.querySelector(".number").textContent = 13;
+// document.querySelector (".score").textContent =10;
+
+// document.querySelecto(".guess").value = 23;
+// console.log(document.querySelector(".guess").value);
+
+
+document.querySelector(".check").sddEventListener("click", function() {
+    console.log(document.querySelector(".guess").value);
+})
+
+// Event handler function

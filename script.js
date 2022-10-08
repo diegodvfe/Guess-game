c2d0571db3ced42af3118f16f52df5580a9cfdb6
Diego Lopez <diego.lopez.19@hotmail.es>
2022-10-08 13:32:13 -0600

    funcitionality with the variable

--- script.js
+++ script.js
@@ -10,8 +10,10 @@
 // document.querySelecto(".guess").value = 23;
 // console.log(document.querySelector(".guess").value);
 
+// This a variable that we use random number that we can validate
 const number = Math.trunc(Math.random()* 20) +1;
 
+//  This is the value of the score that we can decrease the values in the html
 let score = 20;
 document.querySelector(".number") = SecretNumber;
 // Event handler function
@@ -24,13 +26,26 @@ document.querySelector(".check").addEventListener("click", function() {
     } else if (guess === SecretNumber) {
         document.querySelector(".message").textContent = " Correct Number"
     } else if (guess >  SecretNumber) {
-        document.querySelector(".message").textContent = "😵‍💫 Too high"
-        score --;
-        document.querySelector(".score").textContent = score;
+
+        if( score > 1){
+            document.querySelector(".message").textContent = "📣 Too high"
+            score --;
+            document.querySelector(".score").textContent = score;
+        } else {
+            document.querySelector(".message").textContet = "😵 You lost the game, start over"
+            document.querySelector(".score").textContent = 0
+        }
+
     } else if ( guess < SecretNumber){
-        document.querySelector(".message").textContent = " image.png Too Low"
-        score --;
-        document.querySelector(".score").textContent = score;
+
+        if ( score >1) {
+            document.querySelector(".message").textContent = " image.png Too Low"
+            score --;
+            document.querySelector(".score").textContent = score;
+        } else {
+            document.querySelector(".message").textContent = " You lost the game"
+            document.querySelector( ".score").textContent = score --;
+        }
     }
 });
 

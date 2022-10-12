5d2ee3822c1db90d159c3149643c73eb01d1c6c1
Diego Lopez <diego.lopez.19@hotmail.es>
2022-10-12 12:51:24 -0600

    refactoring the funcionality

--- script.js
+++ script.js
@@ -14,6 +14,15 @@ let secretNumber = Math.trunc(Math.random()* 20) +1;
 let score = 20;
 let highscore = 0;
 
+const displayMessage = function(message) {
+    document.querySelector(".message").textContent = message
+}
+
+const numberApp = function(number){
+    document.querySelector(".number").textContent = number;
+}
+
+
 // Event handler function
 document.querySelector(".check").addEventListener("click", function() {
     const guess = Number(document.querySelector(".guess").value);
@@ -21,16 +30,19 @@ document.querySelector(".check").addEventListener("click", function() {
 
 // when there is no input
     if ( !guess ){
-        document.querySelector(".message").textContent = 
-        " No Number";
+        // document.querySelector(".message").textContent = 
+        // " No Number";
+        displayMessage("😑 No Number")
       
         // when player is win
     } else if (guess === secretNumber) {
         document.querySelector(".message").textContent = " Correct Number"
-        document.querySelector(".number").textContent = secretNumber;
+        numberApp(secretNumber)
+        numberApp().style.width = "35rem"
         
         document.querySelector("body").style.backgroundColor = "#60b347"
-        document.querySelector(".number").style.width = "30rem";
+        // document.querySelector(".number").style.width = "30rem";
+
 
         if ( score > highscore){
             highscore = score;
